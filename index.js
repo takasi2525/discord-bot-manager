5896a7a9ce659267e87180194063f6398fab1d01
Yagi Takashi <kookun.sa@gmail.com>
2025-05-17 12:35:15 +0900

    Update Google Auth to use env variables

--- index.js
+++ index.js
@@ -54,12 +54,16 @@ function getCategoryAndType(channelId) {
 }
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: 'credentials.json',
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  },
   scopes: [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
   ]
 });
+
 const sheets = google.sheets({ version: 'v4', auth });
 
 function formatDateFromOption(option) {
